80d6ebd09bd8fc8d1793d0ef2b0619534d0e9628
Marcos Bérgamo <markusdarkness@gmail.com>
2015-10-07 21:17:53 -0300

    Adding cache. Adding utils toolbox.

---
+++ latest/utils.sh
@@ -0,0 +1,49 @@
+#! /bin/bash
+
+contains() {
+  local seeking=$1; shift
+  for el; do
+    if [ "$el" = "$seeking" ]; then
+      return 0
+    fi
+  done
+  return 1
+}
+
+check_cache() {
+  # check the variables necessary to skip setup provider step.
+  SKIP=()
+
+  if [ ! -z ${REMOTE_HOST} ]; then
+      SKIP+=('REMOTE_HOST')
+  fi
+
+  if [ ! -z ${PROVIDER} ]; then
+      SKIP+=('PROVIDER')
+  fi
+}
+
+set_config() {
+  if [ $# -lt 1 ]; then
+    echo "Failed to set config"
+    return 1
+  fi
+
+  echo "$1=\"$2\"" > ${ROOT_PATH}/.config/$1
+}
+
+quiet() {
+  ( $@ ) > /dev/null 2>&1
+}
+
+load_configs() {
+  CONFIG_DIR=${ROOT_PATH}/.config
+
+  if [ ! -d $CONFIG_DIR ]; then
+    mkdir -p $CONFIG_DIR  
+  fi
+
+  for cfg in $( ls ${CONFIG_DIR}/ ); do
+    . ${CONFIG_DIR}/${cfg}
+  done
+}
